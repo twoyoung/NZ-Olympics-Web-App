e54f97aa3cf0f0849890f5f38e4fdc10e81835f6
Yang Yang <yyoung1025@gmail.com>
2023-05-29 14:36:40 +1200

    Add files via upload

---
+++ nzolywebapp/app.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+import re
+from datetime import datetime
+import mysql.connector
+from mysql.connector import FieldType
+import connect
+
+app = Flask(__name__)
+
+dbconn = None
+connection = None
+
+def getCursor():
+    global dbconn
+    global connection
+    connection = mysql.connector.connect(user=connect.dbuser, \
+    password=connect.dbpass, host=connect.dbhost, \
+    database=connect.dbname, autocommit=True)
+    dbconn = connection.cursor()
+    return dbconn
+
+@app.route("/")
+def home():
+    return render_template("base.html")
+
+@app.route("/listmembers")
+def listmembers():
+    connection = getCursor()
+    connection.execute("SELECT * FROM members;")
+    memberList = connection.fetchall()
+    # print(memberList)
+    return render_template("memberlist.html", memberlist = memberList)    
+
+
+@app.route("/listevents")
+def listevents():
+    connection = getCursor()
+    connection.execute("SELECT * FROM events;")
+    eventList = connection.fetchall()
+    return render_template("eventlist.html", eventlist = eventList)
+
