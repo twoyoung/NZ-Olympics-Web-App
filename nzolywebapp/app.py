7c0e7b0fb0e0abdc966431fd021b8b938902869d
YangYang2510 <110264496+YangYang2510@users.noreply.github.com>
2023-06-01 13:31:28 +1200

    Update app.py

--- nzolywebapp/app.py
+++ nzolywebapp/app.py
@@ -30,7 +30,7 @@ def home():
 @app.route("/listmembers")
 def listmembers():
     connection = getCursor()
-    connection.execute("SELECT members.MemberID, teams.TeamID, members.FirstName, members.LastName, members.City, members.Birthdate FROM members FULL OUTER JOIN teams ON teams.TeamId = members.TeamID;")
+    connection.execute("SELECT members.MemberID, teams.TeamID, members.FirstName, members.LastName, members.City, members.Birthdate FROM members FULL OUTER JOIN teams ON teams.TeamID = members.TeamID;")
     memberList = connection.fetchall()
     # print(memberList)
     return render_template("memberlist.html", memberlist = memberList)
