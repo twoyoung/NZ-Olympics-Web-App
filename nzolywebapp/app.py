2ca60c7f70955d44dee1a32626400550ff046d79
YangYang2510 <110264496+YangYang2510@users.noreply.github.com>
2023-06-11 05:00:09 +1200

    Update app.py

--- nzolywebapp/app.py
+++ nzolywebapp/app.py
@@ -39,7 +39,7 @@ def listmembers():
 @app.route("/listevents")
 def listevents():
     connection = getCursor()
-    connection.execute("SELECT EventID, EventName, Sport, TeamName, NZTeam FROM events LEFT JOIN teams ON events.NZTeam = team.TeamID;")
+    connection.execute("SELECT * FROM events;")
     eventList = connection.fetchall()
     return render_template("eventlist.html", eventlist = eventList)
 
@@ -88,7 +88,7 @@ def adminlistmembers():
 @app.route("/admin/listevents")
 def adminlistevents():
     connection = getCursor()
-    connection.execute("SELECT * FROM events;")
+    connection.execute("SELECT EventID, EventName, Sport, TeamName, NZTeam FROM events LEFT JOIN teams ON events.NZTeam = team.TeamID;")
     eventList = connection.fetchall()
     return render_template("admineventlist.html", eventlist = eventList)
 
