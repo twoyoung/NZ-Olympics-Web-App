af2cc4bc225c66d564a95423f6c23c5658eacca7
YangYang2510 <110264496+YangYang2510@users.noreply.github.com>
2023-06-10 23:48:49 +1200

    Update app.py

--- nzolywebapp/app.py
+++ nzolywebapp/app.py
@@ -216,16 +216,16 @@ def stageadd():
     eventname = request.form.get('eventname')
     connection.execute("SELECT EventID FROM events WHERE EventName = %s", (eventname,))
     eventid = connection.fetchone()[0]
-    stagename = request.form.get('stagename')
+    stagename = request.form.get('stagename').lower()
     location = request.form.get('location')
     stagedate = request.form.get('stagedate')
-    if stagename.lower() == 'final':
+    if stagename == 'final':
         qualifying = 0 
     else:
         qualifying = 1
     pointstoqualify = request.form.get('pointstoqualify')
     sql = "INSERT INTO event_stage (StageName, EventID, Location, StageDate, Qualifying, PointsToQualify) VALUES (%s, %s, %s, %s, %s, %s);"
-    parameters = (stagename.lower().capitalize(), eventid, location, stagedate, qualifying, pointstoqualify)
+    parameters = (stagename.capitalize(), eventid, location, stagedate, qualifying, pointstoqualify)
     connection.execute(sql, parameters)
     return redirect("/admin/liststages")
 
