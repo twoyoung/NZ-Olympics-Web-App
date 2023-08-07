369cfc29ee767ceec73e14da74fcd75aa0ae113c
twoyoung <yyoung1025@gmail.com>
2023-08-08 00:03:52 +1200

    dd

--- nzolywebapp/connect.py
+++ nzolywebapp/connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
-dbuser = "2young" #PUT YOUR MySQL username here - usually root
-dbpass = "Young@32494971" #PUT YOUR PASSWORD HERE
-dbhost = "2young.mysql.pythonanywhere-services.com" 
+dbuser = "root" #PUT YOUR MySQL username here - usually root
+dbpass = '' #PUT YOUR PASSWORD HERE
+dbhost = "localhost" 
 dbport = "3306"
-dbname = "2young$COMP636"
+dbname = "nzoly"
