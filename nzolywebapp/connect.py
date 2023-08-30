e8d12878964aba1e0782f7332e13ba0a1f7969a4
Yang Yang <yyoung1025@gmail.com>
2023-08-30 16:36:16 +1200

    Update connect.py

--- nzolywebapp/connect.py
+++ nzolywebapp/connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
 dbuser = "2young" #PUT YOUR MySQL username here - usually root
-dbpass = "Young@32494971" #PUT YOUR PASSWORD HERE
+dbpass = "YouCantGuessIt" #PUT YOUR PASSWORD HERE
 dbhost = "2young.mysql.pythonanywhere-services.com" 
 dbport = "3306"
 dbname = "2young$COMP636"
